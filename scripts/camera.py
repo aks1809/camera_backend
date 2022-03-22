7f681eeb65f2ed99c512109c045d4f2989ed4688
Akshay Chaturvedi <akshay.chaturvedi@frinks.ai>
2022-03-22 18:59:29 +0530

    cahnge frontend

--- scripts/camera.py
+++ scripts/camera.py
@@ -1,16 +1,18 @@
 import cv2
 
-cam = cv2.VideoCapture(0, cv2.CAP_V4L2)
+cam = cv2.VideoCapture(2)
 
 cam.set(cv2.CAP_PROP_FRAME_WIDTH, 3000)
 cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 3000)
-cam.set(cv2.CAP_PROP_EXPOSURE, 2000)
+cam.set(cv2.CAP_PROP_EXPOSURE, 250)
 
 while True:
     ret, frame = cam.read()
     if not ret:
         break
-    cv2.imwrite("/camera_backend/images/upload.bmp", frame)
+    if frame.max() == 0:
+        continue
+    cv2.imwrite("/home/poop/frinks/skh/camera_backend/images/upload.bmp", frame)
     break
 
 cam.release()
